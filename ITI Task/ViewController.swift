6076f0eee67975c19632b10b87beb22f6b92d0ee
Khalid Gad <107983594+imkhalidgad@users.noreply.github.com>
2024-07-30 08:52:02 +0300

    Initial Commit

---
+++ ITI Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITI Task
+//
+//  Created by Khalid Gad on 30/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
